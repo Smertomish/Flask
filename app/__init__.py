9e524d91f411432ae8f7a28406c1437f093d02d7
Smertomish <sasa02149@gmail.com>
2023-03-20 01:03:36 +0300

    альфа-версия механики регистрации добавлена

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-print("haha")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return render_template('success.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
