d5266441c3f1fe0493189271836254af97e6538a
Smertomish <sasa02149@gmail.com>
2023-04-02 20:20:26 +0300

    всем смотреть

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return 'User {} has been registered!'.format(username)
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
